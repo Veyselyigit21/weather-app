0bac113f4086a45e326fa2285687d32e7829bb45
Veysel <veyselyigit142605@gmail.com>
2026-09-25 19:01:21 +0300

    Implement navigation between city list and detail screens

--- app/src/main/java/com/kampplus/hava/MainActivity.kt
+++ app/src/main/java/com/kampplus/hava/MainActivity.kt
@@ -4,7 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.kampplus.hava.core.ui.theme.HavaTheme
+import com.kampplus.hava.feature.weather.presentation.detail.CityDetailScreen
 import com.kampplus.hava.feature.weather.presentation.list.CityListRoute
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,8 +22,35 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             HavaTheme {
-                CityListRoute()
+                HavaAppNavigation()
             }
+        }
+    }
+}
+
+@Composable
+fun HavaAppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "list_screen") {
+        composable("list_screen") {
+            CityListRoute(
+                onCityClick = { cityName ->
+                    navController.navigate("detail_screen/$cityName")
+                }
+            )
+        }
+
+        composable(
+            route = "detail_screen/{cityName}",
+            arguments = listOf(navArgument("cityName") { type = NavType.StringType })
+        ) { backStackEntry ->
+            val clickedCityName = backStackEntry.arguments?.getString("cityName") ?: ""
+            
+            CityDetailScreen(
+                cityName = clickedCityName,
+                onBackClick = { navController.popBackStack() }
+            )
         }
     }
 }
